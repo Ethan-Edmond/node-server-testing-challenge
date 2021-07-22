d8d0007129a8ef455dabb1d61bc54691b0cae58e
Ethan Edmond <eedmond@uccs.edu>
2021-07-22 10:06:33 -0600

    Checking off readme and floundering in tests

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -2,9 +2,11 @@ const request = require('supertest');
 const server = require('../api/server');
 
 describe('server.js', () => {
-  it('sends a 200 status on get / endpoint', async () => {
-    const expectedStatus = 200;
-    const response = await request(server).get('/');
-    expect(response.status).toBe(expectedStatus);
+  describe('/ route', () => {
+    it('sends a 200 status on get / endpoint', async () => { // kinda just a sanity check
+      const expectedStatus = 200;
+      const response = await request(server).get('/');
+      expect(response.status).toBe(expectedStatus);
+    });
   });
 });
